3fb44a14a3feb8f572bbc4351f0bf4321d43da93
Steve-Random <nanafokousteve@gmail.com>
2026-04-13 13:31:25 -0400

    Worked on the Category class

--- src/Category.java
+++ src/Category.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
 public class Category {
     private String categoryName;
-    private double weight;
+    private double weight; // (example: 0.35 for 35%
+
+    ArrayList<Assignment> assignments;
+
+    //Method to return the mean of all assignments for a category
+    public double calculateCategoryAverage() {
+        double sumOfScores = 0, sumOfTotals = 0, mean = 0;
+
+        for (Assignment currentAssignment : assignments) {
+            sumOfScores += currentAssignment.score;
+            sumOfTotals += currentAssignment.maxScore;
+        }
+        mean = sumOfScores / sumOfTotals;
+        return mean;
+    }
 }
