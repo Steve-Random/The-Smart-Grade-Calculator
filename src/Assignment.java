fdcc046f8d6a8ffaf1a5a7dbbb03893df77e643f
Steve-Random <nanafokousteve@gmail.com>
2026-04-13 14:45:50 -0400

    Worked on the Assignment and Category class: added constructors and methods

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,7 +1,27 @@
 public class Assignment {
     // Purpose: To hold data for a single piece of work
-    private String assignmentName;
-    private double score;
-    private double maxScore;
+    private String name;
+    public double score;
+    public double maxScore;
+
+    public Assignment( String name, double score, double maxScore){
+        this.name = name;
+        this.score = score;
+        this.maxScore = maxScore;
+    }
+
+    // Get Methods
+
+    public String getName() {
+        return name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public double getMaxScore() {
+        return maxScore;
+    }
 }
 
